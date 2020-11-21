4b821c17781d3bd0f7d6cb214b22653e443821e4
goreleaserbot <goreleaser@carlosbecker.com>
2020-11-21 20:00:58 +0100

    Brew formula update for homebrew-tap version v0.3.9

--- Formula/pitwall.rb
+++ Formula/pitwall.rb
@@ -2,16 +2,16 @@
 class Pitwall < Formula
   desc ""
   homepage ""
-  version "0.3.8"
+  version "0.3.9"
   bottle :unneeded
 
   if OS.mac?
-    url "https://github.com/atoz-technology/homebrew-tap/releases/download/v0.3.8/homebrew-tap_0.3.8_Darwin_x86_64.tar.gz"
-    sha256 "1c3a9251d2d1e4a8ae2ef3910099a1fdc2b0df5a631b66d90a616b8b5293f2c5"
+    url "https://github.com/atoz-technology/homebrew-tap/releases/download/v0.3.9/homebrew-tap_0.3.9_Darwin_x86_64.tar.gz"
+    sha256 "e66f2755083435723d2a821dccd56efacae4904573e9c66fad4c11c81accd1ce"
   end
   if OS.linux? && Hardware::CPU.intel?
-    url "https://github.com/atoz-technology/homebrew-tap/releases/download/v0.3.8/homebrew-tap_0.3.8_Linux_x86_64.tar.gz"
-    sha256 "940b19827f7b8dbfc0423e1849737610e14313b2822228a7b460a965458ee0d2"
+    url "https://github.com/atoz-technology/homebrew-tap/releases/download/v0.3.9/homebrew-tap_0.3.9_Linux_x86_64.tar.gz"
+    sha256 "fccb56efcc1913a2fe9b5143b9dd4b3edb543b19b67fa8d5e166807ffa12bd11"
   end
 
   def install
